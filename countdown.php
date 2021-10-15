f38a66a4b886111b19ddfbb1c84bc5ce2f0c1208
shani4260 <73388005+shani4260@users.noreply.github.com>
2021-10-15 17:43:47 +0300

    Add files via upload

---
+++ countdown.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="he">
+<head>
+    <meta charset="UTF-8">
+    <title>countdown</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/countdown.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+	<b><p id="countDown"></p></b>
+
+	<script src="../JS/countdown.js"></script>
+	
+</body>
+
+</html>
+
